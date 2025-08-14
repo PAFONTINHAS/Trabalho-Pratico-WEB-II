328a0c8c7d606b66d9ab280851deaac58df415c2
Luigi G. <luigilgirardi@gmail.com>
2025-08-14 19:29:05 -0300

    Criação diretórios de páginas + component solicitar manutenção

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Solicitar } from "./pages/cliente/solicitar/solicitar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Solicitar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
