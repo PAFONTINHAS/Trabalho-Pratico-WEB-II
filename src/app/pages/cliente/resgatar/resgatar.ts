4b14da1306a07b796018610a9efbc18a00536fcc
isabellalcvicente <isabellacadastros16@gmail.com>
2025-08-21 10:52:24 -0300

    feat componente resgatar e estrutura basica html

---
+++ src/app/pages/cliente/resgatar/resgatar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resgatar',
+  imports: [],
+  templateUrl: './resgatar.html',
+  styleUrl: './resgatar.css'
+})
+export class Resgatar {
+
+}
