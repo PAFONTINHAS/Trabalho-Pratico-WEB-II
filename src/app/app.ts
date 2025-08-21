938970658302d04a6047c0a339508fc1bb09cdcb
isabellalcvicente <isabellacadastros16@gmail.com>
2025-08-21 10:55:31 -0300

    testando html de componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,13 @@ import { Solicitar } from "./pages/cliente/solicitar/solicitar";
 import { FuncionarioHomepage } from "./pages/funcionario/funcionario-homepage/funcionario-homepage";
 import { Login } from "./pages/login/login";
 import { PaginaInicial } from "./pages/cliente/pagina-inicial/pagina-inicial";
+import { Detalhes } from './pages/cliente/detalhes/detalhes';
+import { RejeitarAceitar } from './pages/cliente/rejeitar-aceitar/rejeitar-aceitar';
+import { Resgatar } from './pages/cliente/resgatar/resgatar'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Solicitar, FuncionarioHomepage, Login, PaginaInicial],
+  imports: [RouterOutlet, Solicitar, FuncionarioHomepage, Login, PaginaInicial, Detalhes, RejeitarAceitar,Resgatar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
