645763875ede288fb2f2f1e9c98248933d351cb9
gabriela <gabriela.gandine@gmail.com>
2025-08-24 15:34:48 -0300

    rf012-orcamento

---
+++ src/app/pages/funcionario/orcamento/orcamento.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orcamento',
+  imports: [],
+  templateUrl: './orcamento.html',
+  styleUrl: './orcamento.css'
+})
+export class Orcamento {
+
+}
