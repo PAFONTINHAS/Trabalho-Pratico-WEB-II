9703f0194937c7b89fdd43f8857c77c875055311
Clarissa <clarissamorita1@gmail.com>
2025-08-25 18:39:39 -0300

    Visualização de solicitação

---
+++ src/app/pages/funcionario/visualizar-solicitacao/visualizar-solicitacao.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizar-solicitacao',
+  imports: [],
+  templateUrl: './visualizar-solicitacao.html',
+  styleUrl: './visualizar-solicitacao.css'
+})
+export class VisualizarSolicitacao {
+
+}
