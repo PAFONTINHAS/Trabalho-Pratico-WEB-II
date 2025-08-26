682148fa965d52f63ff4a819a9ebe35ea43ab040
Peterson Fontinhas <petersonfontinhas@gmail.com>
2025-08-25 21:40:00 -0300

    Commit to merge

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,37 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Solicitar } from "./pages/cliente/solicitar/solicitar";
-import { FuncionarioHomepage } from "./pages/funcionario/funcionario-homepage/funcionario-homepage";
-import { Login } from "./pages/login/login";
-import { PaginaInicial } from "./pages/cliente/pagina-inicial/pagina-inicial";
-import { Detalhes } from './pages/cliente/detalhes/detalhes';
-import { RejeitarAceitar } from './pages/cliente/rejeitar-aceitar/rejeitar-aceitar';
-import { Resgatar } from './pages/cliente/resgatar/resgatar'
+import { Component, signal }   from '@angular/core';
+import { RouterOutlet }        from '@angular/router';
+import { Login }               from "./pages/login/login";
+import { Detalhes }            from './pages/cliente/detalhes/detalhes';
+import { Resgatar }            from './pages/cliente/resgatar/resgatar';
+import { Solicitar }           from "./pages/cliente/solicitar/solicitar";
+import { Orcamento }           from "./pages/funcionario/orcamento/orcamento";
+import { PaginaInicial }       from "./pages/cliente/pagina-inicial/pagina-inicial";
+import { RejeitarAceitar }     from './pages/cliente/rejeitar-aceitar/rejeitar-aceitar';
+import { MoverManutencao }     from './pages/funcionario/mover-manutencao/mover-manutencao';
 import { VisualizarOrcamento } from './pages/cliente/visualizar-orcamento/visualizar-orcamento';
+import { FinalizarManutencao } from "./pages/funcionario/finalizar-manutencao/finalizar-manutencao";
+import { FuncionarioHomepage } from "./pages/funcionario/funcionario-homepage/funcionario-homepage";
+// import { VisualizarManutencao } from "./pages/funcionario/visualizar-manutencao/visualizar-manutencao";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Solicitar, FuncionarioHomepage, Login, PaginaInicial, Detalhes, RejeitarAceitar,Resgatar, VisualizarOrcamento],
+  imports: [
+    RouterOutlet,
+    Solicitar,
+    FuncionarioHomepage,
+    Login,
+    PaginaInicial,
+    Detalhes,
+    RejeitarAceitar,
+    Resgatar,
+    VisualizarOrcamento,
+    MoverManutencao,
+    Orcamento,
+    FinalizarManutencao
+],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('MM-TADS');
