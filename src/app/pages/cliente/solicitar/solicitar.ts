dc739ed4766266274bbfc3feac83815f6673a9d1
gabriela <gabriela.gandine@gmail.com>
2025-09-19 20:53:36 -0300

    inserção de solicitação

--- src/app/pages/cliente/solicitar/solicitar.ts
+++ src/app/pages/cliente/solicitar/solicitar.ts
@@ -4,10 +4,13 @@ import { Router, RouterLink } from '@angular/router';
 import { EquipamentoService } from '../../../services/equipamento';
 import { EquipamentoComponent } from '../../funcionario/crud-equipamento/crud-equipamento';
 import { Equipamento } from '../../../shared/models/equipamento.model';
+import { Solicitacao } from '../../../shared/models/solicitacao.model';
+import { SolicitacaoService } from '../../../services/solicitacao';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-solicitar',
-    imports: [RouterLink, CommonModule],
+    imports: [RouterLink, CommonModule, FormsModule],
     templateUrl: './solicitar.html',
     styleUrl: './solicitar.css'
 })
@@ -15,12 +18,21 @@ export class Solicitar implements OnInit {
 
     categorias: Equipamento[] = []
     component: EquipamentoService = new EquipamentoService()
+    solicitacao: Solicitacao = new Solicitacao()
     
-      constructor() {}
-    
-      ngOnInit(): void {
-        this.categorias = this.component.listarTodos()    
-      }
+    constructor(
+      private solicitacaoService: SolicitacaoService,
+      private router: Router
+    ) {}
+  
+    ngOnInit(): void {
+      this.categorias = this.component.listarTodos()    
+    }
+
+    inserir(): void {
+      this.solicitacaoService.inserir(this.solicitacao);
+      this.router.navigate( ["/cliente"] );
+    }
     
 }
 
--- src/app/shared/models/solicitacao.model.ts
+++ src/app/shared/models/solicitacao.model.ts
@@ -1,3 +1,4 @@
+import { Status } from "./enums/status.enum";
 import { Equipamento } from "./equipamento.model";
 import { Funcionario } from "./funcionario.model";
 
@@ -5,17 +6,14 @@ export class Solicitacao {
     constructor(
         public id: number = 0,
         public equipamento: Equipamento = new Equipamento(),
-        public status: string = "",
+        public status: Status = Status.Aberta,
         public data: Date = new Date(),
         public orcamento: number = 0,
         public funcionario: Funcionario = new Funcionario(),
         public defeito: string = "",
         public cliente: string = "",
-        public dataSolicitacao: string = ""
-
-        
-   // categoria: Categoria;
-   // status: Status;
-   // historicoStatus: HistoricoStatus[];
+        public dataSolicitacao: string = "",
+        public descricaoEquipamento: string = ""
+      // historicoStatus: HistoricoStatus[];
     ) {}
 }
