77eb7a1d740b6f58f52cffb9266098827e18a48d
isabellalcvicente <isabellacadastros16@gmail.com>
2025-09-20 18:47:43 -0300

    feat: modais de confirmacao gerais criados

---
+++ src/app/pages/cliente/modais-confirmacao/modais-confirmacao.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+@Component({
+  selector: 'app-modais-confirmacao',
+  imports: [],
+  templateUrl: './modais-confirmacao.html',
+  styleUrl: './modais-confirmacao.css'
+})
+export class ModaisConfirmacao {
+  @Input() tipoModalSolicitacao: any;
+  titulo: string="";
+  texto: string="";
+  botaoTexto: string="OK";
+
+  ngOnInit():void{
+
+    switch(this.tipoModalSolicitacao) {
+
+      case "aprovarServico":
+      this.titulo="Serviço aprovado"
+      this.texto="Serviço aprovado no valor de R$300,00"
+
+      case "rejeitarServico":
+        this.titulo="Orçamento Rejeitado"
+        this.texto="Serviço rejeitado com sucesso!"
+
+      case "resgatarServico":
+        this.titulo="Resgate concluído"
+        this.texto="Solicitação de manutenção resgatada com sucesso!"
+
+      case "solicitacaoCriada":
+        this.titulo="Solicitação criada"
+        this.texto="Solicitação de manutenção criada com sucesso!"
+      }
+
+
+    if(this.tipoModalSolicitacao==="aprovarServico"){
+    alert("Modal de solicitacao aprovada");
+  }
+
+  if(this.tipoModalSolicitacao==="rejeitarServico"){
+      alert("Modal de rejeitar serviço");
+  }
+
+  if(this.tipoModalSolicitacao==="resgatarServico"){
+    alert("Modal de resgatar serviço");
+  }
+
+  if(this.tipoModalSolicitacao==="solicitacaoCriada"){
+    alert("Modal de solicitacao criada");
+  }
+
+  if(this.tipoModalSolicitacao==="confirmarRejeicaoServico"){
+    alert("Modal de confirmar Rejeicao Servico");
+  }
+
+  if(this.tipoModalSolicitacao==="resgateConcluido"){
+    alert("Modal resgate oncluido");
+  }
+
+}
+
+}
