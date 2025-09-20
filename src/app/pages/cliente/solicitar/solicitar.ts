327e11c8993554a3de38eab802c4233662f65aa0
isabellalcvicente <isabellacadastros16@gmail.com>
2025-09-20 18:49:06 -0300

    feat: implementacao dos modais de confirmacao

--- src/app/pages/cliente/solicitar/solicitar.ts
+++ src/app/pages/cliente/solicitar/solicitar.ts
@@ -1,16 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { EquipamentoService } from '../../../services/equipamento-service/equipamento';
+import { EquipamentoService } from '../../../services/equipamento-service/equipamento-service';
 import { FormsModule } from '@angular/forms';
 import { Equipamento } from '../../../shared/entities/equipamento_entity';
 import { Solicitacao } from '../../../shared/entities/solicitacao_entity';
 import { SolicitacaoService } from '../../../services/solicitacao_service/solicitacao-service';
 import { solicitacoes } from '../../../../assets/mock/solicitacoes_mocks';
+import { ModaisConfirmacao } from '../modais-confirmacao/modais-confirmacao';
 
 @Component({
     selector: 'app-solicitar',
-    imports: [RouterLink, CommonModule, FormsModule],
+    imports: [RouterLink, CommonModule, FormsModule, ModaisConfirmacao],
     templateUrl: './solicitar.html',
     styleUrl: './solicitar.css'
 })
@@ -19,20 +20,25 @@ export class Solicitar{
     categorias: Equipamento[] = []
     component: EquipamentoService = new EquipamentoService()
     solicitacao = solicitacoes[0];
-    
+    modalSolicitacaoCriada:boolean=false;
+
     constructor(
       private solicitacaoService: SolicitacaoService,
       private router: Router
     ) {}
-  
+
     ngOnInit(): void {
-      this.categorias = this.component.listarTodos()    
+      this.categorias = this.component.listarTodos()
     }
 
     inserir(): void {
       // this.solicitacaoService.inserir(this.solicitacao);
       this.router.navigate( ["/cliente"] );
     }
-    
+
+    solicitacaoCriada(){
+      this.modalSolicitacaoCriada=true;
+    }
+
 }
 
