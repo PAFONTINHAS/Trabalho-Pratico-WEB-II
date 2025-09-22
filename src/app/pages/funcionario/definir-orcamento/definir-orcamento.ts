61e4291c9e145b6f7752ec744f4078b1975222a7
Clarissa <clarissamorita1@gmail.com>
2025-09-22 18:19:18 -0300

    ts confirmar e rejeitar orcamento

--- src/app/pages/funcionario/definir-orcamento/definir-orcamento.ts
+++ src/app/pages/funcionario/definir-orcamento/definir-orcamento.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ModaisConfirmacao } from '../modais-confirmacao/modais-confirmacao';
+import { RejeitarOrcamento } from '../rejeitar-orcamento/rejeitar-orcamento';
 
 @Component({
   selector: 'app-definir-orcamento',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [CommonModule, ModaisConfirmacao, RejeitarOrcamento],
   templateUrl: './definir-orcamento.html',
-  styleUrl: './definir-orcamento.css'
+  styleUrls: ['./definir-orcamento.css']
 })
 export class DefinirOrcamento {
+  modalConfirmacaoAberto = false;
+  modalRejeitarAberto = false;
 
+  abrirModalConfirmacao() {
+    this.modalConfirmacaoAberto = true;
+  }
+
+  fecharModalConfirmacao() {
+    this.modalConfirmacaoAberto = false;
+  }
+
+  abrirModalRejeitar() {
+    this.modalRejeitarAberto = true;
+  }
+
+  fecharModalRejeitar() {
+    this.modalRejeitarAberto = false;
+  }
 }
+
