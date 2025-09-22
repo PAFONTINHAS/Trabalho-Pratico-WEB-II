58986691f4f6dc82c86d17f0a5882515051cc4de
Clarissa <clarissamorita1@gmail.com>
2025-09-22 16:46:18 -0300

    atualização

--- src/app/pages/funcionario/confirmar-orcamento/confirmar-orcamento.ts
+++ src/app/pages/funcionario/confirmar-orcamento/confirmar-orcamento.ts
@@ -1,16 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-detalhes-solicitacao',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './confirmar-orcamento.html',
   styleUrls: ['./confirmar-orcamento.css']
 })
 export class ConfirmarOrcamento {
   mostrarModal: boolean = false;
   valorOrcamento: number = 300; // exemplo
+mostrarModalConfirmacao: any;
 
   abrirModalConfirmacao() {
     this.mostrarModal = true;
