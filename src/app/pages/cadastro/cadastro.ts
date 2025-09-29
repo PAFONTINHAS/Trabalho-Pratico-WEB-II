a1beb976fdfb29a718fd0d1f4b646e889af2357a
Luigi G. <luigilgirardi@gmail.com>
2025-09-29 14:50:18 -0300

    feat: serviço de API viaCep no cadastro

--- src/app/pages/cadastro/cadastro.ts
+++ src/app/pages/cadastro/cadastro.ts
@@ -1,14 +1,57 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ViaCep } from '../../services/via-cep/via-cep';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css'
 })
 export class Cadastro {
+
+  cadastroForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private viaCepService: ViaCep
+  ) {
+    this.cadastroForm = this.fb.group({
+      cpf: [''],
+      nome: [''],
+      email: [''],
+      cep: [''],
+      uf: ['PR'], 
+      cidade: [''],
+      logradouro: [''],
+      numero: [''],
+      telefone: ['']
+    });
+  }
+
+  consultaCEP() {
+    const cep = this.cadastroForm.get('cep')?.value;
+
+    if (cep && cep.length >= 8) { 
+      this.viaCepService.consultarCep(cep).subscribe(dados => this.populaDadosForm(dados));
+    }
+  }
+
+  populaDadosForm(dados: any) {
+    if (dados.erro) {
+      alert('CEP não encontrado.');
+      return;
+    }
+
+    this.cadastroForm.patchValue({
+      logradouro: dados.logradouro,
+      cidade: dados.localidade,
+      uf: dados.uf
+    });
+  }
+
   handlePhone (event: any) {
     console.log("oi")
     let input = event.target
---
+++ src/app/services/via-cep/via-cep.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ViaCep } from './via-cep';
+
+
+describe('ViaCep', () => {
+  let service: ViaCep;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ViaCep);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
