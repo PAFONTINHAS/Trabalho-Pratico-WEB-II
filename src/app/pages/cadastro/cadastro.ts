72272ad78bd9aad82a98b9adfe551623102d9fbb
gabriela <gabriela.gandine@gmail.com>
2025-09-28 21:56:53 -0300

    mascara input cadastro

--- src/app/pages/cadastro/cadastro.ts
+++ src/app/pages/cadastro/cadastro.ts
@@ -9,5 +9,38 @@ import { RouterLink } from '@angular/router';
   styleUrl: './cadastro.css'
 })
 export class Cadastro {
+  handlePhone (event: any) {
+    console.log("oi")
+    let input = event.target
+    input.value = this.phoneMask(input.value)
+  }
+
+  private phoneMask = (value: string) => {
+    if (!value) return ""
+
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+
+    return value
+  }
+
+  handleCpf(e: any) {
+    let input = e.target
+    input.value = this.cpfMask(input.value)
+
+  }
+
+  private cpfMask = (value: any) => {
+    if (!value) return ""
+
+    value = value.replace(/\D/g, '') 
+    value = value.replace(/(\d{3})(\d)/, '$1.$2')
+    value = value.replace(/(\d{3})(\d)/, '$1.$2') 
+    value = value.replace(/(\d{3})(\d)/, '$1-$2')
+    value = value.replace(/(-\d{2})\d+?$/, '$1')
+
+    return value
+  }
 
 }
