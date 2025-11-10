d771a41ac01a54af47b7543011204831ddb3d277
isabellalcvicente <isabellacadastros16@gmail.com>
2025-11-10 11:27:55 -0300

    fix: erro de caseSensitive

--- src/app/shared/functions/telefone_valido.ts
+++ src/app/shared/functions/telefone_valido.ts
@@ -1,7 +1,7 @@
 import { AbstractControl } from "@angular/forms";
 
 export function telefoneValido(control: AbstractControl): { [key: string]: boolean } | null {
-  const telefone = control.value?.replaceALl(/\D/g, '');
+  const telefone = control.value?.replaceAll(/\D/g, '');
   if (telefone && (telefone.length === 10 || telefone.length === 11)) {
     return null;
   }
