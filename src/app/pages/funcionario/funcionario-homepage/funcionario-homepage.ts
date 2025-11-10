5f1bb55cd23c53af94f02af87abc1aa56b45127f
Clarissa <clarissamorita1@gmail.com>
2025-11-09 23:43:10 -0300

    Funcionario homepage status

--- src/app/pages/funcionario/funcionario-homepage/funcionario-homepage.ts
+++ src/app/pages/funcionario/funcionario-homepage/funcionario-homepage.ts
@@ -4,6 +4,7 @@ import { VisualizarSolicitacao } from '../visualizar-solicitacao/visualizar-soli
 import { NgClass } from '@angular/common';
 import { Solicitacao } from '../../../shared/entities/solicitacao_entity';
 import { SolicitacaoService } from '../../../services/solicitacao_service/solicitacao-service';
+import { Status } from '../../../shared/models/enums/status.enum';
 
 @Component({
   selector: 'app-funcionario-homepage',
@@ -11,10 +12,11 @@ import { SolicitacaoService } from '../../../services/solicitacao_service/solici
   templateUrl: './funcionario-homepage.html',
 })
 export class FuncionarioHomepage implements OnInit{
-
   solicitacoes: Solicitacao[] = [];
+  statusEnum = Status; // ADICIONADO: Para usar no template
+  
   constructor( private readonly solicitacaoService: SolicitacaoService){}
-
+  
   carregarSolicitacoes(){
     this.solicitacaoService.listarTodos().subscribe({
       next: (data) =>{
@@ -26,47 +28,45 @@ export class FuncionarioHomepage implements OnInit{
       }
     });
   }
-
+  
   countAbertas(): number {
-    return this.solicitacoes.filter(solicitacao => solicitacao.status.toUpperCase() === 'ABERTA').length;
+    return this.solicitacoes.filter(solicitacao => solicitacao.status === Status.Aberta).length;
   }
-
+  
   ngOnInit(): void {
     this.carregarSolicitacoes();
   }
-
+  
   mostrarModalSolicitacao = false;
   solicitacaoSelecionada?: Solicitacao | null = null;
-
-
+  
   abrirModal(solicitacao: Solicitacao): void {
     this.solicitacaoSelecionada = solicitacao;
     this.mostrarModalSolicitacao = true;
   }
-
+  
   fecharModal(): void {
     this.mostrarModalSolicitacao = false;
     this.solicitacaoSelecionada = null;
   }
   
-  statusClasses(status: String) {
-    
-    switch (status.toUpperCase()) {
-      case 'ABERTA': 
+  statusClasses(status: Status): object {
+    switch (status) {
+      case Status.Aberta: 
        return {'cursor-default bg-gray-100 px-2 py-1 border-1 border-gray-400 rounded-lg text-gray-600': true};
-      case 'ORÇADA':
+      case Status.Orcada:
         return {'cursor-default bg-yellow-700/20 px-2 py-1 border-1 border-yellow-800 rounded-lg text-yellow-800': true};
-      case 'APROVADA':
+      case Status.Aprovada:
         return {'cursor-default bg-yellow-100 px-2 py-1 border-1 border-yellow-400 rounded-lg text-yellow-500': true};
-      case 'REJEITADA':
+      case Status.Rejeitada:
         return {'cursor-default bg-red-100 px-2 py-1 border-1 border-red-400 rounded-lg text-red-600': true};
-      case 'REDIRECIONADA':
+      case Status.Redirecionada:
         return {'cursor-default bg-purple-100 px-2 py-1 border-1 border-purple-400 rounded-lg text-purple-600': true};
-      case 'ARRUMADA':
+      case Status.Arrumada:
         return {'cursor-default bg-blue-100 px-2 py-1 border-1 border-blue-400 rounded-lg text-blue-600': true};
-      case 'PAGA':
+      case Status.Paga:
         return {'cursor-default bg-orange-100 px-2 py-1 border-1 border-orange-400 rounded-lg text-orange-600': true};
-      case 'FINALIZADA':
+      case Status.Finalizada:
         return {'cursor-default bg-green-100 px-2 py-1 border-1 border-green-400 rounded-lg text-green-600': true};
       default:
         return {'cursor-default bg-gray-100 px-2 py-1 border-1 border-gray-400 rounded-lg text-gray-600': true}
