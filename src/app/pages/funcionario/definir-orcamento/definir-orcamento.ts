97f60a1670ea4af2c4674acd86d4d5efd5248f53
isabellalcvicente <isabellacadastros16@gmail.com>
2025-11-18 11:11:04 -0300

    fix: definicao do valor do orç que ignorava virgula

--- src/app/pages/funcionario/definir-orcamento/definir-orcamento.ts
+++ src/app/pages/funcionario/definir-orcamento/definir-orcamento.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ModaisConfirmacao } from '../modais-confirmacao/modais-confirmacao';
 import { RejeitarOrcamento } from '../rejeitar-orcamento/rejeitar-orcamento';
 import { RouterLink } from '@angular/router';
-
+import { SolicitacaoService } from '../../../services/solicitacao_service/solicitacao-service';
 @Component({
   selector: 'app-definir-orcamento',
   standalone: true,
@@ -11,9 +11,13 @@ import { RouterLink } from '@angular/router';
   templateUrl: './definir-orcamento.html',
   styleUrls: ['./definir-orcamento.css']
 })
+
 export class DefinirOrcamento {
+  constructor(private solicitacaoService: SolicitacaoService){}
   modalConfirmacaoAberto = false;
   modalRejeitarAberto = false;
+  orcamento: string="";
+  solicitacao: any;
 
   abrirModalConfirmacao() {
     this.modalConfirmacaoAberto = true;
@@ -34,16 +38,29 @@ export class DefinirOrcamento {
   handleOrcamento(e: any) {
     let input = e.target
     input.value = this.orcamentoMask(input.value)
+    this.orcamento=input.value; //salva o valor com mascara
   }
 
   orcamentoMask(value: any) {
-    
-    value = value.replace(/\D/g, '') 
+    if (!value) return "";
+    value = value.replace(/\D/g, '')
     value = value.replace(/(\d+)(\d{2})$/, "$1,$2"); // Adiciona a parte de centavos
     value = value.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."); // Adiciona pontos a cada três dígitos
     console.log(value)
 
     return value
+  }
+
+  salvarOrcamento(solicitacao:any){
+    const valorBackend = this.orcamento.replace(/\./g, '')//remove pontos do milhar
+    .replace(',', '.');//troca , decimal por ponto
+    solicitacao.valorOrcamento=Number(valorBackend);
+    //seta o valor convertido
+    this.solicitacaoService.atualizar(solicitacao).subscribe({
+      next:()=>console.log("Orçamento atualizado"),
+      error:(err)=> console.error("erro ao atualizar orç",err)
+    });
+
   }
 
 }
